3cd55bbf3d79567288b51a681b3eecef35c837f8
tomasjgiron <47337943+tomasjgiron@users.noreply.github.com>
2019-04-01 04:49:59 -0300

    Funciones y main del TP 1

---
+++ clase04/funciones.h
@@ -0,0 +1,18 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+long utn_factorial(int numberA);
+
+int utn_sum(int numberA, int numberB);
+
+int utn_substraction(int numberA, int numberB);
+
+int utn_multiply(int numberA, int numberB);
+
+float utn_division(int numberA, int numberB, char* msgError);
+
+int utn_getInt(int *pNum, char* msg, char* msgError);
+
+int utn_getFloat(float *pNum, char* msg, char* msgError);
+
+#endif // FUNCIONES_H_INCLUDED
--- clase04/main.c
+++ clase04/main.c
@@ -1,15 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "utn.h"
-
+#include "funciones.h"
 
 int main()
 {
-    int num;
-    utn_getNumberRanged(&num,100,0,"Ingrese un numero del 0 al 100: ","Error. ",3);
-    if(num>0 && num<100)
+    char seguir='s';
+    int opcion=0;
+    int A;
+    int B;
+    int result;
+
+    while(seguir=='s')
     {
-        printf("Es correcto. El numero ingresado es: %d\n",num);
-    }
+        printf("1- Ingresar 1er operando (A=x)\n");
+        printf("2- Ingresar 2do operando (B=y)\n");
+        printf("3- Calcular la suma (A+B)\n");
+        printf("4- Calcular la resta (A-B)\n");
+        printf("5- Calcular la division (A/B)\n");
+        printf("6- Calcular la multiplicacion (A*B)\n");
+        printf("7- Calcular el factorial (A!)\n");
+        printf("8- Calcular todas las operaciones\n");
+        printf("9- Salir\n");
+
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+            case 1:
+                utn_getInt(&A,"Ingrese primer operando: ","Error");
+                break;
+            case 2:
+                utn_getInt(&B,"Ingrese segundo operando: ","Error");
+                break;
+            case 3:
+                result = utn_sum(A,B);
+                printf("La suma es: %d",result);
+                break;
+            case 4:
+                result = utn_substraction(A,B);
+                printf("La diferencia es: %d",result);
+                break;
+            case 5:
+                result = utn_division(A,B,"Error");
+                printf("La división es: %d",result);
+                break;
+            case 6:
+                result = utn_multiply(A,B);
+                printf("La multiplicación es: %d",result);
+                break;
+            case 7:
+                result = utn_factorial(A);
+                printf("El factorial de %d es: %d",A ,result);
+                break;
+            case 8:
+                break;
+            case 9:
+                seguir = 'n';
+                break;
+        }
+
+
     return 0;
+}
 }
